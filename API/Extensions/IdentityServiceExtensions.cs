940333123a420d76e900d97b06f7e47923e55e82
Nathan <Creeds123@hotmail.com>
2022-05-13 21:33:11 +1000

    CodeFactor Review
    Removed left over test code and blank lines

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -10,11 +10,6 @@ using System.Text;
 
 namespace API.Extensions
 {
-    public class UserRole : IdentityRole
-    {
-        
-    }
-
     public static class IdentityServiceExtensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
