c60bb4dcab1de0ecc23c0254d42d0b3acc890f3d
Nathan <Creeds123@hotmail.com>
2022-05-15 00:36:56 +1000

    Updated RolesController.cs (#3)
    Added Query to retrieve roles assigned to a user.

---
+++ Application/Roles/Queries/Details.cs
@@ -0,0 +1,41 @@
+﻿using Application.Core;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Roles.Queries
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<List<string>>>
+        {
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<string>>>
+        {
+            private readonly UserManager<AppUser> _userManager;
+
+            public Handler(UserManager<AppUser> userManager)
+            {
+                _userManager = userManager;
+            }
+
+            public async Task<Result<List<string>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByNameAsync(request.Username);
+
+                if (user == null) return Result<List<string>>.Failure($"Unable to find user: {request.Username}");
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles == null || !roles.Any())
+                {
+                    return Result<List<string>>.Failure($"{request.Username} has no roles.");
+                }
+
+                return Result<List<string>>.Success(roles.ToList());
+            }
+        }
+    }
+}
