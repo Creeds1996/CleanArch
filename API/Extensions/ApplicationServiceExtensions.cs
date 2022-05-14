ffedb779d194103f9e2287bbeac0c112695925fe
Nathan <Creeds123@hotmail.com>
2022-05-14 21:00:36 +1000

    Add Authorization to Swagger
    After logging in successfully using Swagger, you can retrieve your JWT. Scroll to the top and hit the 'Authorize' button in the top right. Enter in the following format 'Bearer {Token}', replacing {Token} with the JWT and hitting the 'Authorize' button will authorize the user to access the endpoints they have permission to execute.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -17,6 +17,26 @@ namespace API.Extensions
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
             services.AddDbContext<DataContext>(options =>
             {
